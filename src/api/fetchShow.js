410ac0465aaf6c874f445287315b3b69fe2702be
Joe Bay <jbay55278@gmail.com>
2020-08-17 14:07:44 -0800

    finished tests for the episodes

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -5,6 +5,7 @@ import axios from 'axios'
  const fetchShow = () => {
     return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
       .then(res => {
+          console.log(res)
          return res
         });
   };
